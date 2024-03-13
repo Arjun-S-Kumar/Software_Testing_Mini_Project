7535bde5ef040f757d31e108c8eddaaa7ebb624e
Arjun-S-Kumar <154949731+Arjun-S-Kumar@users.noreply.github.com>
2024-03-13 20:40:32 +0530

    update

---
+++ Automation-Test-LinkedIn/HashTagTest.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.linkedin.com/")
+
+search = driver.find_element("id","session_key")
+search.send_keys("bcanhck29@gmail.com")
+time.sleep(1)
+search = driver.find_element("id","session_password")
+search.send_keys("qwerty12345")
+time.sleep(1)
+search.submit()
+time.sleep(60)
+driver.get("https://www.linkedin.com/mynetwork/network-manager/hashtags/")
+time.sleep(10)
