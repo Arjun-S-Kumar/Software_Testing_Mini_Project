a1de804ebf83f441422cd82d027adc1abb6998cc
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-03-13 12:28:02 +0530

    update

---
+++ Automation-Test-LinkedIn/ScrollTest.py
@@ -0,0 +1,27 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.linkedin.com/")
+
+search = driver.find_element("id","session_key")
+search.send_keys("bcanhck29@gmail.com")
+time.sleep(1)
+search = driver.find_element("id","session_password")
+search.send_keys("qwerty12345")
+time.sleep(1)
+search.submit()
+time.sleep(60)
+driver.execute_script("window.scrollTo(0,25;")
+
+time.sleep(0.5)
+
+new_height = driver.execute_script("return document.body.scrollHeight")
+
+
+
+time.sleep(20)
+driver.close()
+
+
