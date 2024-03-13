e0b3b84a9dcf410c05eaf942ee2f49ce413ef3fa
Arjun-S-Kumar <154949731+Arjun-S-Kumar@users.noreply.github.com>
2024-03-13 10:52:13 +0530

    update

---
+++ Automation-Test-LinkedIn/ViewProfileTest.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.linkedin.com/")
+
+search = driver.find_element("id","session_key")
+search.send_keys("bcanhck29@gmail.com")
+time.sleep(3)
+search = driver.find_element("id","session_password")
+search.send_keys("qwerty12345")
+time.sleep(3)
+search.submit()
+time.sleep(60)
+search = driver.find_element("xpath","/html/body/div[5]/div[3]/div/div/div[2]/div/div/div/div/div[1]/div[1]/a/div[2]")
+search.click()
+
+
+
+time.sleep(100)
+driver.close()
