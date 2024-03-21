1932e43fe454a119ef16b9a92d3ec0f68aded7f1
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-03-21 20:41:57 +0530

    minor update

--- Automation-Test-LinkedIn/ScrollTest.py
+++ Automation-Test-LinkedIn/ScrollTest.py
@@ -1,21 +1,29 @@
-import time
 from selenium import webdriver
+import allure
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Firefox()
 
-driver.get("https://www.linkedin.com/")
+@allure.severity(allure.severity_level.NORMAL)
 
-search = driver.find_element("id","session_key")
-search.send_keys("bcanhck29@gmail.com")
-time.sleep(1)
-search = driver.find_element("id","session_password")
-search.send_keys("qwerty12345")
-time.sleep(1)
-search.submit()
-time.sleep(60)
-driver.execute_script("window.scrollTo(0,25;")
+class TestGoogleSearch:
 
-time.sleep(0.5)
+    driver = webdriver.Firefox()
+
+    driver.get("https://www.linkedin.com/")
+
+    search = driver.find_element("id","session_key")
+    search.send_keys("bcanhck29@gmail.com")
+    time.sleep(1)
+    search = driver.find_element("id","session_password")
+    search.send_keys("qwerty12345")
+    time.sleep(1)
+    search.submit()
+    time.sleep(60)
+    driver.execute_script("window.scrollTo(0,25;")
+
+    time.sleep(0.5)
 
 new_height = driver.execute_script("return document.body.scrollHeight")
 
